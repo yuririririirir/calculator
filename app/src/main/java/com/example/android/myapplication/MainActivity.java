7dab03415fd84ce042e48873b8346d11bbd842fa
yuririririirir <qladbflzz@naver.com>
2015-01-11 18:16:00 +0900

    TextView,Button초기화, 이벤터헨들러구현, 버튼의 헨들러연결,test

---
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.android.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private TextView textView;
+    private Button btnModify;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.txtInput);
+        btnModify = (Button) findViewById(R.id.btnModify);
+
+
+        View.OnClickListener Listener= new View.OnClickListener(){
+            public void onClick(View v) {
+                if(v.getId()==R.id.btnModify)
+                {
+                    textView.setText("버튼이 눌렸습니다");
+                }
+            }
+         };
+        btnModify.setOnClickListener(Listener);
+//        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/android/myapplication/calculator.java
@@ -0,0 +1,10 @@
+package com.example.android.myapplication;
+
+/**
+ * Created by 김유리 on 2015-01-11.
+ */
+public class calculator {
+    public calculator(){
+
+    }
+}
